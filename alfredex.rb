7329996a0cc9e40215f49ed6bf6821a8709468d1
Xac Stegner <xac@me.com>
2013-11-20 14:45:21 -0500

    Update to support multiple pokemon at a time, separated by comma or forward slash

--- alfredex.rb
+++ alfredex.rb
@@ -7,32 +7,50 @@ class Alfredex
   def self.search(string)
     matches = nil
 
-    if string =~ /^[0-9]+$/ # numeric string, check pokemon number
-      matches = FILE_DATA.select do |name, data|
-        data['number'].to_i == string.to_i
-      end
-    else # check for name
-      matches = FILE_DATA.select do |name, data|
-        name.downcase.include? string.downcase
-      end
-      matches = matches.sort_by do |k,v|
-        k.downcase[0..string.length-1] == string.downcase ? 0 : 1
-      end
-    end
+    if string =~ /[\/,\,]/ # contains separators, look up multiple
+      mons = string.split(/[\/,\,]/).map{|n| n.strip.downcase }
 
-    feedback = Feedback.new
+      matches = FILE_DATA.select do |name, data|
+        name.downcase =~ /#{mons.join('|')}/
+      end
 
-    matches.each do |name, data|
+      feedback = Feedback.new
       feedback.add_item({
-        :title => "##{data['number'].to_i} #{name}",
-        :subtitle => data['description'],
-        :arg => data['url_name'],
-        :uid => data['number'],
-        :icon => {:type => "filetype", :name => "pokemon_sprites/#{data['number']}.png"}
+        :title => "Find Multiple Pokemon",
+        :subtitle => matches.map{|m| m[0]}.join(', '),
+        :arg => matches.map{|m| m[1]['url_name'] }.join(","),
+        :uid => matches.map{|m| m[1]['number'] }.join(','),
+        :icon => {:type => "filetype", :name => "icon.png"}
       })
-    end
 
-    puts feedback.to_xml
+      puts feedback.to_xml
+    else # single pokemon
+      if string =~ /^[0-9]+$/ # numeric string, check pokemon number
+        matches = FILE_DATA.select do |name, data|
+          data['number'].to_i == string.to_i
+        end
+      else # check for name
+        matches = FILE_DATA.select do |name, data|
+          name.downcase.include? string.downcase
+        end
+        matches = matches.sort_by do |k,v|
+          k.downcase[0..string.length-1] == string.downcase ? 0 : 1
+        end
+      end
+
+      feedback = Feedback.new
+      matches.each do |name, data|
+        feedback.add_item({
+          :title => "##{data['number'].to_i} #{name}",
+          :subtitle => data['description'],
+          :arg => data['url_name'],
+          :uid => data['number'],
+          :icon => {:type => "filetype", :name => "pokemon_sprites/#{data['number']}.png"}
+        })
+      end
+
+      puts feedback.to_xml
+    end
   end
 end
 
